7410af52bf0a561bd86923f252189010aa5755b4
mrGafurov <ozodbekgafurovv@gmail.com>
2024-05-24 09:30:41 +0500

    changed sign in state

--- src/views/auth/SignIn/SignInForm.js
+++ src/views/auth/SignIn/SignInForm.js
@@ -40,7 +40,6 @@ const SignInForm = (props) => {
         const result = await signIn({ userName, password })
         if (result?.status === 'failed') {
             setMessage(result.message)
-            window.location.reload();
         } else {
             setMessage("Login yoki Parol noto'g'ri kiritildi");
         }
